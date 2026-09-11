b7f565880f850fbb55b105e874ff075ae5b649e7
elizabeth <elizabethchan2583@gmail.com>
2026-09-11 14:30:13 +0800

    Wk 2 - SmartInventoryAuditor

---
+++ Week 2/SmartInventoryAuditor.py
@@ -0,0 +1,16 @@
+inventory = int(0)
+
+while True:
+    userInput = input("Enter stock quantity or Exit: ")
+    if userInput == "Exit":
+        break
+
+    if not userInput.isnumeric():
+        print("Invalid input, please enter a positive number.")
+    else:
+        inventory += int(userInput)
+        print("Inventory:", inventory)
+        if inventory >= 500:
+            print("Inventory is full, please stop adding stock.")
+            break
+     
