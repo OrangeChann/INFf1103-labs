03a4d00d34ab77413e2b551f8916d51236074743
elizabeth <elizabethchan2583@gmail.com>
2026-09-22 21:22:59 +0800

    Refactor input handling and enhance output reporting in modular audit

--- wk 3/modular_auditor.py
+++ wk 3/modular_auditor.py
@@ -3,6 +3,7 @@ fail = 0
 delivery_count = 0
 
 def get_valid_input():
+    global fail
     while True:
         userInput = input("Enter stock quantity or Exit: ")
         if userInput == "Exit":
@@ -35,12 +36,10 @@ while True:
     if value is None:
         fail+=1
         break
-    print("Total inventory:", inventory, "| Number of errors:", fail)
-
     inventory = process_delivery(inventory, value)
     delivery_count += 1
     tax = calculate_tax(value)
-    print("Inventory:", inventory, "| Tax on this delivery:", tax)
+    print("Total inventory:", inventory, "| Tax on this delivery:", tax, "| Number of errors:", fail, "| Delivery count:", delivery_count)
 
     if inventory > 500:
         print("Print Inventory is full, please stop adding stock.")
